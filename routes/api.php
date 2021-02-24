e5f2f68e8825cef684695b0e5b235fa76a213890
saidgamih <said.gamih@gmail.com>
2021-02-24 15:08:45 +0100

    Adding jwt

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,9 +17,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
-Route::get('/categories', [CategoryController::class, 'index'])->name('category.index');
+// Custom stuff
+
+Route::middleware('auth:api')->group(
+    function () {
+        Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+        Route::get('/categories', [CategoryController::class, 'index'])->name('category.index');
+        // Route::get('/payload', function() {
+        //     return auth()->payload()->toArray();
+        // });
+    }
+);
